d7a21d89af38038bd56a3927b52834c8664fbd6c
= <=>
2025-01-02 00:01:41 +0630

    databaseconnect

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,61 @@
-export const signup = (req , res ) => {
-    res.json({
-        data: "you hit the signup endpoint"
-    })
+import User from "../models/userModel.js"
+import bcrypt from "bcryptjs"
+
+
+export const signup = async (req , res ) => {
+    try{
+        const {username, fullname, email, password} = req.body
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if(!emailRegex.test(email)) {
+            return res.status(400).json({ error: "Invalid email format "});
+
+        }
+
+        const takenUsername = await User.findOne({ username })
+        if(takenUsername) {
+            return res.status(400).json({ error: " Username is already taken "  })
+        }
+        const usedEmail = await User.findOne({ email })
+
+        if(usedEmail) {
+            return res.status(400).json({ error: " email is already taken "  })
+        }
+        //hash password 
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User({
+            fullname,
+            username,
+            email, 
+            password: hashedPassword
+        })
+
+        if(newUser){
+            generateTokenAndSetCoookie(newUser._id, res)
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullname: newUser.fullname,
+                username: newUser.username,
+                email: newUser.email,
+                followers: newUser.followers,
+                following: newUser.following,
+                profileImg: newUser.profileImg,
+                coverImg: newUser.coverImg
+            })
+        } else {
+            res.status(400).json({ error: "Invalid user data "})
+        }
+
+
+    } catch(error){
+
+    }
 }
 
 export const login = (req , res ) => {
--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,24 @@ import express from "express"
 
 import authRouter from "./routes/auth.routes.js"
 
-
-
+import { connectDB } from "./config/db.js";
+import dotenv  from "dotenv"
 
 const app = express();
 
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
 app.get("/", (req, res) => {
     res.send("Server uploaded successfully")
 })
 
 app.use("/api/auth", authRouter)
 
-app.listen(8000,()=> {
-    console.log("server started at 8000")
+app.use(express.json());
+
+app.listen(PORT,()=> {
+    connectDB()
+    console.log(`server started at ${PORT}`)
 })
